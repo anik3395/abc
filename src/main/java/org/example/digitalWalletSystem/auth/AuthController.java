d3ac83473ad6f0e513a95444311b7e06c6c78dfe
anik <anikdas18bd@gmail.com>
2025-08-20 18:38:27 +0600

    First commit

--- src/main/java/org/example/digitalWalletSystem/auth/AuthController.java
+++ src/main/java/org/example/digitalWalletSystem/auth/AuthController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-//    @PostMapping(value = EndPointsUtils.ADMIN_SIGNUP)
-//    public ResponseEntity
+    @PostMapping(value = EndPointsUtils.ADMIN_SIGNUP)
+    public ResponseEntity<String> get(){
+        return null;
+    }
 }
